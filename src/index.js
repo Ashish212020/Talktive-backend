59ac70a67d3d378550a59e0d9b445a3f319f40c9
Ashish212020 <tiwariashish01332@gmail.com>
2025-11-25 03:09:15 +0530

    cors handled

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieparser());
 app.use(
     cors({
-        origin:["http://localhost:5173" , "http://localhost:5174"],
+        origin:["http://localhost:5173" , "http://localhost:5174","https://talktive-frontend.onrender.com"],
         credentials:true,
        
     })
